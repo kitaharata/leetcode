67fe3936c87d0b80d225a1ebc01fc5bb1d53378e
Takuro Kitahara <kitaharata@outlook.jp>
2019-08-02 10:41:18 +0900

    Create boilerplate.go

---
+++ boilerplate.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "math"
+import "strings"
+import "strconv"
+
+func leetcode(x bool) bool {
+	return x
+}
+
+func main() {
+	fmt.Println(leetcode(false))
+	fmt.Println(math.Pi)
+	fmt.Println(strings.Title("false"))
+	fmt.Println(strconv.IntSize)
+}
