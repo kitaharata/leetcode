2f464bccfb436a2284e0cf58e4daf04510b4148f
Takuro Kitahara <kitaharata@outlook.jp>
2019-08-23 02:13:34 +0900

    Update 0088-merge-sorted-array.go

--- go/0088-merge-sorted-array.go
+++ go/0088-merge-sorted-array.go
@@ -1,7 +1,5 @@
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	i := m - 1
-	j := n - 1
-	k := m + n - 1
+	i, j, k := m-1, n-1, m+n-1
 	for i >= 0 && j >= 0 {
 		if nums1[i] >= nums2[j] {
 			nums1[k] = nums1[i]
